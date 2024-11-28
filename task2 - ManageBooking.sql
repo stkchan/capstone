3df872687685362693db9ac1af7295e28da1ed9f
stkchan <stkza007hod@hotmail.com>
2024-11-28 12:23:11 +0700

    add new

---
+++ task2 - ManageBooking.sql
@@ -0,0 +1,3 @@
+CREATE PROCEDURE ManageBooking(IN BookingDate DATE, IN TableNumber INT)
+SELECT BookingDate, TableNumber
+WHERE exists (SELECT * from Booking where Date = BookingDate and TableNumber = TableNumber)
